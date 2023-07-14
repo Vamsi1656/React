d57c3dcb444bb5de1240ce765de9166a1593915d
Vamsi1656 <vamsireddy1717@gmail.com>
2023-07-14 15:37:21 +0530

    Class Components

--- All Src modules/src/Class components/Emp.js
+++ All Src modules/src/Class components/Emp.js
@@ -1,10 +1,20 @@
 import React from "react";
+import Message from './Message'
 class Emp extends React.Component{
-
+           e_name="Rahul"
+           e_id=101
+           e_sal=45000
     render() {
         return (
             <div>
                 <h3>Emp Component</h3>
+                <h3>Name:{this.e_name}</h3>
+                <h3>ID:{this.e_id}</h3>
+                <h3>Sal:{this.e_sal}</h3>
+
+                <hr />
+                <Message/>
+
             </div>
         )
     }
