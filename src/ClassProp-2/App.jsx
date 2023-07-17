23be77f0cdbe97003b61d80c90a7530e97f19e66
Vamsi1656 <vamsireddy1717@gmail.com>
2023-07-17 14:55:48 +0530

    Class

--- src/ClassProp-2/App.jsx
+++ src/ClassProp-2/App.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
-
+import CompA from './CompA'
 const App = () => {
+     
   return (
     <div>
-      
+      <h2>App Component</h2>
+      <hr />
+      <CompA/>
       
     </div>
   )
