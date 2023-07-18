9c26032793c4951323525bc2a32b94113d785ffc
Vamsi1656 <vamsireddy1717@gmail.com>
2023-07-18 18:17:13 +0530

    okie

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 // import React from "react"
 import ReactDOM  from "react-dom"
 import  '../node_modules/bootstrap/dist/css/bootstrap.css'
-import App from "./State-2/App"
+import App from "./State-3/App"
 ReactDOM.render(<App/>,document.getElementById('root'))
 // ReactDOM.render(<A/>,document.getElementById('root'))
 // ReactDOM.render(<App/>,document.getElementById('root'))
