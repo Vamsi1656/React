cd673f9c128066b4744d50968a45027ea0130bb8
Vamsi1656 <vamsireddy1717@gmail.com>
2023-07-22 13:11:28 +0530

    okie

--- src/Axios/Users.jsx
+++ src/Axios/Users.jsx
@@ -1,15 +1,20 @@
+// Axios is a library that serves to create HTTP requests that are present externally. 
+//It is evident from the fact that we may sometimes in React applications need to get data from the external source. 
+//It is quite difficult to fetch such data so that they can be normally shown on the website.
+
+
 import React, { Component } from 'react'
 import axios from 'axios'
 class Users extends Component {
-    state={
-        users:[]
-    }
+    state={ 
+        User:[]
+    };
     getHandler=()=>{
             //invoking API
-        axios.get("https://jsonplaceholder.typicode.com/users")
+        const response= axios.get("https://jsonplaceholder.typicode.com/users")
         .then((response)=>{
             console.log(response)
-            this.setState({users:response})
+            this.setState({User:response.data})
         })
         .catch((err)=>{console.log(err)})   
     }
@@ -17,7 +22,25 @@ class Users extends Component {
     return (
       <div>
          <button onClick={this.getHandler}>Click</button>
-         <pre>{JSON.stringify(this.state    )}</pre>
+         <pre>{JSON.stringify(this.state)}</pre>
+         <table className='table'  >
+          <thead>
+            <th>ID</th>
+            <th>Name</th>
+            <th>city</th>
+          </thead>
+          <tbody>
+            {
+              this.state.User.map((value)=>{
+                return (<tr>
+                  <td>{value.id}</td>
+                  <td>{value.name}</td>
+                  <td>{value.address.city}</td>
+                </tr>);
+              })
+            }
+          </tbody>
+         </table>
       </div>
      
     )
