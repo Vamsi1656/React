72c56b0498dcc7f8dc635e127cdf55939d64515e
Vamsi1656 <vamsireddy1717@gmail.com>
2023-07-24 16:44:50 +0530

    Axios

---
+++ src/Axios-3/Product.jsx
@@ -0,0 +1,48 @@
+// ComponentDidMount is a method which executes automatically after rendor method
+
+import React, { Component } from 'react'
+import axios from 'axios'
+class Product extends Component {
+            state={
+              products:[]
+            }
+            componentDidMount(){
+              axios.get("https://dummyjson.com/products")
+                .then((response)=>{
+                      console.log(response)
+                      this.setState({products:response.data.products})
+                })
+                .catch((err)=>{console.log(err)})
+            }
+  render() {
+    return (
+      <div>
+        <h3>Products Component</h3>
+        <pre>{JSON.stringify(this.state.products)}</pre>
+        <table className='table'>
+          <thead>
+            <th>Id</th>
+            <th>Title</th>
+            <th>Image</th>
+          </thead>
+          <tbody>
+            {
+              this.state.products.map((value,index)=>{
+                return(
+                  <tr key={index}>
+                    <td>{value.id}</td>
+                    <td>{value.title}</td>
+                    <td><img src={value.images[0]} width={200} height={150} alt="" /></td>
+
+                  </tr>
+                )
+              })
+            }
+          </tbody>
+        </table>
+        </div>
+    )
+  }
+}
+
+export default Product
