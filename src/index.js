aa75b2ad1cce5b346e0e732fbb82d6a32bc7e4b7
Vamsi1656 <vamsireddy1717@gmail.com>
2023-08-08 16:44:36 +0530

    okie

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 // import React from "react"
 import ReactDOM  from "react-dom"
-import App from './DataFilter/App'
+import App from './Login/App'
 ReactDOM.render(<App/>,document.getElementById('root'))
